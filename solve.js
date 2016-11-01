48bf7352cf03653cfbabe370e04f262974d965bf
jchromik <Jonas.Chromik@student.hpi.uni-potsdam.de>
2016-11-01 10:30:24 +0100

    SYNC  M solve.html; M solve.js;

--- solve.js
+++ solve.js
@@ -7,14 +7,24 @@ function solve(actualInput, transformation, targetOutput) {
   
   var difference = Math.abs(actualOutput - targetOutput);
   var targetInput = actualInput;
-  while(difference >= 0.1) {
-    var newDifference = Math.abs(transformation(targetInput+1) - targetOutput);
-    if(newDifference < difference) {
-      targetInput++;
-    } else {
-      targetInput--;
+  var stepWidth = 1.0;
+  var direction = +1; // D = {-1, +1}
+  var counter = 0;
+  while(difference >= 0.001 || counter < 1000) {
+    
+    var newDifference = Math.abs(transformation(targetInput+(stepWidth*direction)) - targetOutput);
+    
+    // if we head in the wrong direction
+    if(newDifference > difference) { 
+      direction = direction * (-1);
+      if(counter > 0) {
+        // do not adjust stepWidth in first run
+        stepWidth = stepWidth * 0.1;
+      }
     }
+    targetInput = targetInput + direction*stepWidth;
     difference = Math.abs(transformation(targetInput) - targetOutput);
+    counter = counter+1;
   }
   return targetInput;
 }
