384d1686334197e1fe33c6c3a68bf0613ed8a866
jchromik <Jonas.Chromik@student.hpi.uni-potsdam.de>
2016-11-02 12:44:09 +0100

    SYNC  M solve2.js;

--- solve2.js
+++ solve2.js
@@ -7,48 +7,40 @@ function solve2(actualInput, transformation, targetOutput) {
   for(var i in actualInput) {
     // modifying single variable
     var influenced = new Set();
-    var 
+    var tries = [];
     var diffInput = Object.assign({}, actualInput);
     switch(typeof(diffInput[i])) {
       case "boolean":
-        diffInput[i] = !diffInput[i];
+        tries = [!actualInput[i]];
         break;
       case "number":
-        [-1, 0, 1, actualInput[i]+1, actualInput[i]-1].forEach(function(j) {
-          diffInput[i] = j;
-          findInfluencedVariables(actualOutput, diffInput, transformation).forEach(function(k) {
-            influenced.add(k);
-          });
-        });
-        diffInput[i] = diffInput[i] + 1;
+        tries = [-1, 0, 1, actualInput[i]+1, actualInput[i]-1];
         break;
       case "string":
-        diffInput[i] = diffInput[i] + "1";
+        tries = ["", "hello", "13", "\n", diffInput[i] + "1"];
         break;
       default:
         break;
     }
+    
+    tries.forEach(function(j) {
+      diffInput[i] = j;
+      var diffOutput = transformation(diffInput);
   
-    dependencies[i] = findInfluencedVariables(actualOutput, diffInput, transformation);
+      // collecting keys
+      var keys = new Set(Object.keys(diffOutput));
+      Object.keys(actualOutput).forEach(function(key) { keys.add(key) });
+  
+      // searching for influenced variables
+      keys.forEach(function(key) { 
+        if(actualOutput[key] != diffOutput[key]) {
+          influenced.add(key);
+        }
+      });
+    });
+  
+    dependencies[i] = influenced;
   }
   
   console.log(dependencies);
-}
-
-function findInfluencedVariables(actualOutput, diffInput, transformation) {
- var diffOutput = transformation(diffInput);
-  
-  // collecting keys
-  var keys = new Set(Object.keys(diffOutput));
-  Object.keys(actualOutput).forEach(function(key) { keys.add(key) });
-  
-  // searching for influenced variables
-  var influenced = [];
-  keys.forEach(function(key) { 
-    if(actualOutput[key] != diffOutput[key]) {
-      influenced.push(key);
-    }
-  });
-  
-  return influenced;
 }
