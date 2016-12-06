aa9f6785cb867059cf9a3a4a854b8b9f923925b1
jchromik <jonas.chromik@student.hpi.de>
2016-12-06 12:19:05 +0100

    resolve merge conflict

--- tests/test-solve-partial.js
+++ tests/test-solve-partial.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-suite('Array', function() {
-  setup(function() {
-    // ...
-  });
-
-  suite('#indexOf()', function() {
-    test('should return -1 when not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-});
-=======
 var expect = chai.expect;
 
 describe("Blub", function() {
@@ -24,4 +11,3 @@ describe("Blub", function() {
     });
   });
 });
->>>>>>> 3b14ebff3fe954693741ce47cb68f7ffc0dc623f
