3e798ef40245af28460bab12dec7955e55f1255c
jchromik <Jonas.Chromik@student.hpi.uni-potsdam.de>
2016-12-06 15:54:08 +0100

    SQUASHED: SQUASHED-AUTO-COMMIT-tests-solve-partial-test.js,

--- tests/solve-partial-test.js
+++ tests/solve-partial-test.js
@@ -1,13 +1,21 @@
-import {expect} from '../node_modules/chai/chai.js'
-import {..} from '../solve-partial.js'
+import {expect} from 'node_modules/chai/chai.js'
+import {solveForNumberToNumber} from '../solve-partial.js'
 
 describe("Numbers 1 to 1 relationship", function() {
   it("negation", function() {
     var negate = function(x) { return -x; };
     expect(solveForNumberToNumber(1, negate, 5)).to.equal(-5);
+    expect(solveForNumberToNumber(20, negate, 5)).to.equal(-5);
+    expect(solveForNumberToNumber(1, negate, -6)).to.equal(6);
+    expect(solveForNumberToNumber(-1, negate,-6)).to.equal(6);
+    // TODO: Does not work accurately for floats.
   });
 
   it("increment", function() {
-    expect("failing").to.equal("not failing");
+    var increment = function(x) { return x+1; };
+    expect(solveForNumberToNumber(1, increment, 4)).to.equal(3);
+    expect(solveForNumberToNumber(4, increment, 4)).to.equal(3);
+    expect(solveForNumberToNumber(1, increment, -2)).to.equal(-3);
+    expect(solveForNumberToNumber(-1, increment, -2)).to.equal(-3);
   });
 });
