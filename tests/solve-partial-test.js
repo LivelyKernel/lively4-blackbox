a8ff49f9c1e54afd0eb23431d0217ebb36e8a61b
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2016-12-06 15:33:09 +0100

    SQUASHED: AUTO-COMMIT-tests-solve-partial-test.js,Merge-branch-master-of-https-github.com-LivelyKernel-lively4-blackbox,

---
+++ tests/solve-partial-test.js
@@ -0,0 +1,15 @@
+import {expect} from 'node_modules/chai/chai.js'
+
+// var expect = chai.expect;
+
+describe("Blub", function() {
+  describe("test", function() {
+    it("passing", function() {
+      expect("cow.name").to.equal("cow.name");
+    });
+
+    it("failing", function() {
+      expect("cow.name").to.equal("Kate");
+    });
+  });
+});
