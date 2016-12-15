c935a497a318cab400cf9d9d1d06c7447c651210
chistopher <christopher.weyand@student.hpi.de>
2016-12-15 16:14:41 +0100

    write flexible tests
    SQUASHED: AUTO-COMMIT-tests-solve-partial-test.js,

--- tests/solve-partial-test.js
+++ tests/solve-partial-test.js
@@ -1,84 +1,90 @@
+import {assert} from 'node_modules/chai/chai.js'
 import {expect} from 'node_modules/chai/chai.js'
 import '../solve-partial.js'
 
 describe("Integers", function() {
   describe("1 to 1 relationship", function() {
     it("negation", function() {
-      var negate = function(x) { return -x[0]; };
-      expect(solveForNumberToNumber(-1, negate, 5)).to.equal(-5);
-      expect(solveForNumberToNumber(1, negate, 5)).to.equal(-5);
-      expect(solveForNumberToNumber(20, negate, 5)).to.equal(-5);
-      expect(solveForNumberToNumber(1, negate,-6)).to.equal(6);
-      expect(solveForNumberToNumber(-1, negate, -6)).to.equal(6);
-      expect(solveForNumberToNumber(-20, negate,-6)).to.equal(6);
+      var transf = function(x) { return -x[0]; };
+      var inputs = [-1, 1,20, 1,-1];
+      var target = [ 5, 5, 5,-6,-6];
+      for(i=0;i<inputs.length;++i){
+        var solution = solveForNumberToNumber([inputs[i]], transf, target[i]);
+        var variance = Math.abs(transf(solution) - target[i]);
+        expect(variance).to.be.at.most(0);
+      }
     });
   
     it("increment", function() {
-      var increment = function(x) { return x[0]+1; };
-      expect(solveForNumberToNumber(-1, increment, 4)).to.equal(3);
-      expect(solveForNumberToNumber(1, increment, 4)).to.equal(3);
-      expect(solveForNumberToNumber(6, increment, 4)).to.equal(3);
-      expect(solveForNumberToNumber(1, increment, -2)).to.equal(-3);
-      expect(solveForNumberToNumber(-1, increment, -2)).to.equal(-3);
-      expect(solveForNumberToNumber(-6, increment, -2)).to.equal(-3);
+      var transf = function(x) { return x[0]+1; };
+      var inputs = [-1, 1, 6, 1,-1];
+      var target = [ 4, 4, 4,-2,-2];
+      for(i=0;i<inputs.length;++i){
+        var solution = solveForNumberToNumber([inputs[i]], transf, target[i]);
+        var variance = Math.abs(transf(solution) - target[i]);
+        expect(variance).to.be.at.most(0);
+      }
     });
     
     it("double", function() {
-      var double = function(x) { return x[0]*2; };
-      expect(solveForNumberToNumber(-5, double, 4)).to.equal(2);
-      expect(solveForNumberToNumber(5, double, 4)).to.equal(2);
-      expect(solveForNumberToNumber(10, double, 4)).to.equal(2);
-      expect(solveForNumberToNumber(5, double, -2)).to.equal(-1);
-      expect(solveForNumberToNumber(-5, double, -2)).to.equal(-1);
-      expect(solveForNumberToNumber(-10, double, -2)).to.equal(-1);
+      var transf = function(x) { return x[0]*2; };
+      var inputs = [-5, 5,10, 5,-5];
+      var target = [ 4, 4, 4,-2,-2];
+      for(i=0;i<inputs.length;++i){
+        var solution = solveForNumberToNumber([inputs[i]], transf, target[i]);
+        var variance = Math.abs(transf(solution) - target[i]);
+        expect(variance).to.be.at.most(0);
+      }
     });
   });
   
-  describe("n to 1 relationship", function() { /* not yet implemented */ });
+  //describe("n to 1 relationship", function() { /* not yet implemented */ });
 });
+
+
 
 describe("Floats", function() {
   describe("1 to 1 relationship", function() {
     it("negation", function() {
-      var negate = function(x) { return -x[0]; };
-      expect(solveForNumberToNumber(-1.0, negate, 2.5)).to.equal(-2.5);
-      expect(solveForNumberToNumber(1.0, negate, 2.5)).to.equal(-2.5);
-      expect(solveForNumberToNumber(20.0, negate, 2.5)).to.equal(-2.5);
-      expect(solveForNumberToNumber(20.05, negate, 2.5)).to.equal(-2.5);
-      expect(solveForNumberToNumber(1.0, negate,-6.2)).to.equal(6.2);
-      expect(solveForNumberToNumber(-1.0, negate, -6.2)).to.equal(6.2);
-      expect(solveForNumberToNumber(-20.0, negate,-6.2)).to.equal(6.2);
-      expect(solveForNumberToNumber(-20.05, negate,-6.2)).to.equal(6.2);
+      var transf = function(x) { return -x[0]; };
+      var inputs = [ -1.0, 1.0, 20.0, 20.05,  1.0, -1.0, -20.0, -20.05];
+      var target = [  2.5, 2.5,  2.5,   2.5, -6.2, -6.2,  -6.2,   -6.2];
+      for(i=0;i<inputs.length;++i){
+        var solution = solveForNumberToNumber([inputs[i]], transf, target[i]);
+        var variance = Math.abs(transf(solution) - target[i]);
+        expect(variance).to.be.at.most(0);
+      }
     });
-  
+
+    
     it("increment", function() {
-      var increment = function(x) { return x[0]+1; };
-      expect(solveForNumberToNumber(-1.0, increment, 4.5)).to.equal(3.5);
-      expect(solveForNumberToNumber(1.0, increment, 4.5)).to.equal(3.5);
-      expect(solveForNumberToNumber(6.0, increment, 4.5)).to.equal(3.5);
-      expect(solveForNumberToNumber(6.05, increment, 4.5)).to.equal(3.5);
-      expect(solveForNumberToNumber(1.0, increment, -2.2)).to.equal(-3.2);
-      expect(solveForNumberToNumber(-1.0, increment, -2.2)).to.equal(-3.2);
-      expect(solveForNumberToNumber(-6.0, increment, -2.2)).to.equal(-3.2);
-      expect(solveForNumberToNumber(-6.05, increment, -2.2)).to.equal(-3.2);
+      var transf = function(x) { return x[0]+1; };
+      var inputs = [ -1.0, 1.0, 6.0, 6.05,  1.0, -1.0, -6.0, -6.05];
+      var target = [  4.5, 4.5, 4.5,  4.5, -2.2, -2.2, -2.2,  -2.2];
+      for(i=0;i<inputs.length;++i){
+        var solution = solveForNumberToNumber([inputs[i]], transf, target[i]);
+        var variance = Math.abs(transf(solution) - target[i]);
+        expect(variance).to.be.at.most(0);
+      }
     });
     
     it("double", function() {
-      var double = function(x) { return x[0]*2; };
-      expect(solveForNumberToNumber(-5.0, double, 4.5)).to.equal(2.25);
-      expect(solveForNumberToNumber(5.0, double, 4.5)).to.equal(2.25);
-      expect(solveForNumberToNumber(10.0, double, 4.5)).to.equal(2.25);
-      expect(solveForNumberToNumber(10.05, double, 4.5)).to.equal(2.25);
-      expect(solveForNumberToNumber(5.0, double, -2.2)).to.equal(-1.1);
-      expect(solveForNumberToNumber(-5.0, double, -2.2)).to.equal(-1.1);
-      expect(solveForNumberToNumber(-10.0, double, -2.2)).to.equal(-1.1);
-      expect(solveForNumberToNumber(-10.05, double, -2.2)).to.equal(-1.1);
-    });
+      var transf = function(x) { return x[0]*2; };
+      var inputs = [ -5.0, 5.0, 10.0, 10.05, 5.0, -5.0, -10.0, -10.05];
+      var target = [  4.5, 4.5,  4.5,   4.5,-2.2, -2.2,  -2.2,   -2.2]
+      for(i=0;i<inputs.length;++i){
+        var solution = solveForNumberToNumber([inputs[i]], transf, target[i]);
+        var variance = Math.abs(transf(solution) - target[i]);
+        expect(variance).to.be.at.most(0);
+      }
+    }); 
   });
   
-  describe("n to 1 relationship", function() { /* not yet implemented */ });
+  describe("n to 1 relationship", function() {  });
+  
 });
 
+/*
 describe("String", function() {
   describe("1 to 1 relationship", function() {
     it("reverse -- input with correct length", function() {
@@ -110,5 +116,7 @@ describe("String", function() {
     });
   });
   
-  describe("1 to 1 relationship", function() { /* not yet implemented */ });
+  describe("1 to 1 relationship", function() {  });
 });
+*/
+
