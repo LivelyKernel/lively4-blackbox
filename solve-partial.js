826c8dff6cb95b7954ef8dab4edf911466e0598f
jchromik <Jonas.Chromik@student.hpi.uni-potsdam.de>
2017-01-06 15:35:46 +0100

    SQUASHED: AUTO-COMMIT-solve-partial.html,AUTO-COMMIT-solve-partial.js,

--- solve-partial.js
+++ solve-partial.js
@@ -1,4 +1,7 @@
-// assumption: input and output are integers
+/* Ressources on Genetic.js:
+ * https://github.com/subprotocol/genetic-js/blob/master/examples/string-solver.html
+ * http://subprotocol.com/system/genetic-js.html
+ */
 
 function solveForNumberToNumber(actualInput, transformation, targetOutput) {
   var actualOutput = transformation(actualInput);
@@ -136,20 +139,105 @@ function solveForStringToString(actualInput, transformation, targetOutput) {
   
   genetic.evolve(config, userData);
 
-  
   return prom;
   
-  /* Ressources:
-   * https://github.com/subprotocol/genetic-js/blob/master/examples/string-solver.html
-   * http://subprotocol.com/system/genetic-js.html
-   */
 }
 
+function solveForStringToNumber(actualInput, transformation, targetOutput) {
+  
+  var genetic = Genetic.create();
+  
+  var config = {
+		"iterations": 4000,
+		"size": 250,
+		"crossover": 0,
+		"mutation":1,
+		"skip": 20,
+	};
 
-/* HELPERS */
+  var userData = {
+    "actualInput": actualInput[0],
+    "transformation": transformation,
+    "targetOutput": targetOutput
+  };
+  
+  genetic.optimize = Genetic.Optimize.Minimize;
+  genetic.select1 = Genetic.Select1.Tournament2;
+  genetic.select2 = Genetic.Select2.Tournament2;
 
-function rectifyFloat(number, stepWidth) {
-  return parseFloat(number.toFixed(((1/stepWidth) % 10) + 1));
+  genetic.seed = function() {
+    // evolution starts off with given input
+    return this.userData["actualInput"];
+  };
+  
+  genetic.mutate = function(entity) {
+  	function replaceAt(str, index, character) {
+		  return str.substr(0, index) + character + str.substr(index+character.length);
+	  }
+	  
+	  
+	  //lengthening and shortening
+	  var rand = Math.random();
+	  if(rand < 0.1 ){ //shorten
+	    return entity.substr(0, entity.length - 1);
+	  }
+	  if(rand < 0.2){ //lengthen
+	    return entity + 'e';
+	  }
+	  
+	
+  	// chromosomal drift
+	  var i = Math.floor(Math.random()*entity.length)		
+	  return replaceAt(entity, i, String.fromCharCode(entity.charCodeAt(i) + (Math.floor(Math.random()*2) ? 1 : -1)));
+  };
+  
+  genetic.crossover = function(mother, father) {
+	  /* two-point crossover
+	  var len = mother.length;
+	  var ca = Math.floor(Math.random()*len);
+	  var cb = Math.floor(Math.random()*len);		
+	  if (ca > cb) {
+	  	var tmp = cb;
+	  	cb = ca;
+	  	ca = tmp;
+	  }
+	  	
+	  var son = father.substr(0,ca) + mother.substr(ca, cb-ca) + father.substr(cb);
+  	var daughter = mother.substr(0,ca) + father.substr(ca, cb-ca) + mother.substr(cb);
+  	
+  	*/
+	  return [father, mother];
+  };
+  
+  genetic.fitness = function(entity) {
+    var opt = this.userData["targetOutput"];
+    var act = (this.userData["transformation"](entity));
+    
+    var fitness = Math.abs(opt - act);
+    
+    return fitness;
+  };
+  
+  
+  genetic.generation = function(pop, generation, stats) {
+	  // stop running once we've reached the solution OR after 1000 generations
+	  return this.userData["transformation"](pop[0].entity) != this.userData["targetOutput"];
+  };
+  
+  var prom = new Promise((resolve, reject) => {
+    genetic.notification = function(pop, generation, stats, isFinished) {
+      if(isFinished) {
+        resolve(pop[0].entity);
+        console.log(stats);
+        console.log(generation);
+      }
+    };
+  });
+  
+  genetic.evolve(config, userData);
+
+  return prom;
+
 }
 
 
@@ -168,7 +256,11 @@ function solveForManyNumbers(actualInputArray, transformation, targetOutput) {
   return new Promise((resolve, reject) => {
     resolve(solution);
   });
+  
+}
 
+/* HELPERS */
 
-
+function rectifyFloat(number, stepWidth) {
+  return parseFloat(number.toFixed(((1/stepWidth) % 10) + 1));
 }
